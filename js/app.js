ce8a0c1339dfe8ecd43be94b11e8b45455e86758
Sebs <sebastianruizj2014@gmail.com>
2023-09-02 19:26:55 -0500

    Organización
    Se unificó la función para mostrar las cartas y que solo se necesite un FOR para mostrar las cartas.
    Precios para los productos.
    Mejor estructurado el HTML

--- js/app.js
+++ js/app.js
@@ -1,68 +1,64 @@
 const listaDeProductos = [
-    ["Camisetas de algodón de varios colores y diseños", "camisanegra.jpeg"],
-    ["Pantalones vaqueros de diferentes estilos y tallas", "camisa1.jpeg"],
-    ["Camisetas deportivas para hombres","camisa2.jpeg"],
-    ["Pantalones de vestir para mujeres","camisa3.jpeg"],
-    ["Camisetas de manga larga para niños","camisa5.jpeg"],
-    ["Pantalones cortos de verano para mujeres","camisa9.jpeg"],
-    ["Camisas elegantes para hombres","camisa6.jpeg"],
-    ["Pantalones casuales para hombres","camisa7.jpeg"],
+    ["Camisetas de algodón de varios colores y diseños", "camisanegra.jpeg", "$5000.00"],
+    ["Pantalones vaqueros de diferentes estilos y tallas", "camisa1.jpeg", "$2000.00"],
+    ["Camisetas deportivas para hombres","camisa2.jpeg", "$1500.00"],
+    ["Pantalones de vestir para mujeres","camisa3.jpeg", "$9000.00"],
+    ["Camisetas de manga larga para niños","camisa5.jpeg", "$2000.00"],
+    ["Pantalones cortos de verano para mujeres","camisa9.jpeg", "$1200.00"],
+    ["Camisas elegantes para hombres","camisa6.jpeg", "$9000.00"],
+    ["Pantalones casuales para hombres","camisa7.jpeg", "$1000.00"],
 ];
-
 const listaDeAcces = [
-    ["Cadena rayo", "rayo.jpeg"],
-    ["Cadena ", "cadena.jpeg"],
-    ["cadena de gatos", "amorgato2.jpeg"],
-    ["anillos para parejas", "anillosparejas.jpeg"],
-    ["aretes", "arete.jpeg"],
-    ["anillos", "3_negroplata.jpeg"],
-    ["anillo emociones", "emociones.jpeg"],
-    ["Pulpsereas", "pulsera.jpeg"]
+    ["Cadena rayo", "rayo.jpeg", "$2000.00"],
+    ["Cadena ", "cadena.jpeg", "$1000.00"],
+    ["Cadena de gatos", "amorgato2.jpeg", "$500.00"],
+    ["Anillos para parejas", "anillosparejas.jpeg", "$10000.00"],
+    ["Aretes", "arete.jpeg", "$100.00"],
+    ["Anillos", "3_negroplata.jpeg", "$50000.00"],
+    ["Anillo emociones", "emociones.jpeg", "$5000.00"],
+    ["Pulseras", "pulsera.jpeg", "$100.00"]
 ];
 
-/*-- Productos --*/
-const nombresProductos = listaDeProductos.map(producto => producto[0]);
-const imagenesProductos = listaDeProductos.map(producto => producto[1]);
+const divProductos = document.getElementById('ver-productos');
+const divAccesorios = document.getElementById('ver-accesorios');
 
-let divProductos = document.getElementById('ver-productos');
-for (let i = 0; i < 8; i++) {
-    const divCarta = document.createElement('div')
-    divCarta.className = 'cards'
-    divProductos.append(divCarta);
-    const img = document.createElement('img');
-    img.src = "../media/img/ropa/"+imagenesProductos[i]
-    img.alt = "No existe"
-    divCarta.append(img)
-    const parrafo = document.createElement('p')
-    divCarta.append(parrafo)
-    parrafo.innerHTML = nombresProductos[i]
-    const enlace = document.createElement('a')
-    enlace.href = "VistaProducto.html"
-    enlace.innerHTML = "Visitar Producto"
-    divCarta.append(enlace)
-}
+mostrarCartas(listaDeProductos, divProductos, "ropa");
+mostrarCartas(listaDeAcces, divAccesorios, "accesorio");
 
-/*-- Accesorios --*/
-const nombresAccesorios = listaDeAcces.map(i => i[0]);
-const imagenesAccesorios = listaDeAcces.map(i => i[1]);
+function mostrarCartas(array, divId, tipo) {
+    const arrayNombres = array.map(e => e[0])
+    const arrayImagenes = array.map(e => e[1])
+    const arrayPrecios = array.map(e => e[2])
 
-let divAccesorios = document.getElementById('ver-accesorios');
-for (let i = 0; i < 8; i++) {
-    const divCarta = document.createElement('div')
-    divCarta.className = 'cards'
-    divAccesorios.append(divCarta);
-    const img = document.createElement('img');
-    img.src = "../media/img/"+imagenesAccesorios[i]
-    img.alt = "No existe"
-    divCarta.append(img)
-    const parrafo = document.createElement('p')
-    divCarta.append(parrafo)
-    parrafo.innerHTML = nombresAccesorios[i]
-    const enlace = document.createElement('a')
-    enlace.href = "VistaProducto.html"
-    enlace.innerHTML = "Visitar Producto"
-    enlace.className = "btn"
-    divCarta.append(enlace)
+    for(let i = 0; i < 8; i++) {
+        /* Se crea un div para contenedor de las cartas*/
+        let divCarta = document.createElement('div');
+        divCarta.className = 'cards';
+        divId.append(divCarta);
+
+        /* Se crea una etiqueta <img> y se referencia */
+        let img = document.createElement('img');
+        img.src = "../media/img/"+tipo+"/"+arrayImagenes[i];
+        img.alt = "No existe";
+        divCarta.append(img);
+
+        /* Se crea una etiqueta <p> y se muestra el texto */
+        let parrafo = document.createElement('p');
+        divCarta.append(parrafo);
+        parrafo.innerHTML = arrayNombres[i];
+
+        /* Se crea una etiqueta <p> y se muestra el precio */
+        let precio = document.createElement('p');
+        divCarta.append(precio);
+        precio.innerHTML = arrayPrecios[i];
+
+        /* Se crea una etiqueta <a> y se crea un enlace a otra página */
+        let enlace = document.createElement('a');
+        enlace.href = "producto.html";
+        enlace.innerHTML = "Visitar Producto",
+        enlace.className = "btn";
+        divCarta.append(enlace);
+    }
 }
 
 /*-- Slider --*/
